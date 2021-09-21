641ddabe17e7c9f1428a40de06d9e8b4182620fd
FlorianDuchesne <florian.duchesne@gmail.com>
2021-09-21 10:34:44 +0200

    responsive

--- public/js/script.js
+++ public/js/script.js
@@ -464,3 +464,24 @@ jQuery(function () {
   });
   console.log("slick actif");
 });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("entete");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  console.log("scroll");
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
